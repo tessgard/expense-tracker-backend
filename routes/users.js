9b303f0ec84de76aaa03c100e8c7765857003c3e
Tess Gard <tessgard@Tesss-MacBook-Pro.local>
2019-10-22 11:20:48 +1100

    user authentication

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,71 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
 
 const User = require('../models/User');
 
 // @route         POST api/users
 // @description   Register a user
 // @access        Public
-router.post('/', (req, res) => {
-  res.send(req.body);
-});
+router.post(
+  '/',
+  // validation
+  [
+    check('name', 'Name is required')
+      .not()
+      .isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'Please set a password with 6 or more characters'
+    ).isLength({ min: 6 })
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+      if (user) {
+        return res.status(400).json({ msg: 'user already exists' });
+      }
+
+      user = new User({
+        name,
+        email,
+        password
+      });
+
+      // password encryption
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      await user.save();
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 36000
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (error) {}
+  }
+);
 
 module.exports = router;
